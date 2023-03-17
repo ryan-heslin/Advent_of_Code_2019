cb93bfa523c08789746c82ff1a072d96122be159
ryan-heslin <rwheslin@gmail.com>
2023-03-16 20:14:08 -0400

    Day 16

--- solutions/day16.R
+++ solutions/day16.R
@@ -1,14 +1,3 @@
-raw_input <- readLines("inputs/day16.txt") |>
-    strsplit(split = "") |>
-    unlist(use.names = FALSE) |>
-    as.integer()
-
-leading_digits <- function(x) {
-    x <- abs(x)
-    x %/% 10^floor(log(x, 10))
-}
-
-
 naive_fft <- function(input, iterations, pattern) {
     size <- length(input)
     patterns <- vapply(seq_len(size),
@@ -23,8 +12,41 @@ naive_fft <- function(input, iterations, pattern) {
     input
 }
 
-pattern <- c(0, 1, 0, -1)
+# We just have to backsolve up to the target digit each iteration, since
+# the trailing half of digits
+# *are all independent* of the first half
+backsolve <- function(input, target, iterations = 100) {
+    target <- target + 1 # number of digits to skip before
+    size <- length(input)
+    repetitions <- 10000
+    end <- size * repetitions
+    start <- max(target %% size, 0)
+
+
+    # partial vector starting from target end position, then repeat to max length
+    vector <- c(
+        input[start:size],
+        rep(input, length.out = (size * repetitions - target - (size - start)))
+    ) |>
+        rev()
+    endpoint <- length(vector)
+
+    for (iter in seq_len(iterations)) {
+        vector <- abs(cumsum(vector)) %% 10
+    }
+    tail(vector, 8) |>
+        rev() |>
+        as.character() |>
+        paste(collapse = "")
+}
+
+raw_input <- readLines("inputs/day16.txt") |>
+    strsplit(split = "") |>
+    unlist(use.names = FALSE) |>
+    as.integer()
+
 # raw_input <- 1:8
+pattern <- c(0, 1, 0, -1)
 result <- naive_fft(raw_input, 100, pattern)
 part1 <- result[1:8] |>
     as.character() |>
@@ -32,4 +54,8 @@ part1 <- result[1:8] |>
 print(part1)
 
 offset <- as.character(raw_input[1:7]) |>
+    paste(collapse = "") |>
     as.integer()
+
+part2 <- backsolve(raw_input, offset)
+print(part2)
